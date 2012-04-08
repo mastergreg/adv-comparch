d3b0a6a95ae2bc81e5b13d6c880f9434e488bd4b
master <master@lemmy.(none)>
2012-04-08 23:51:55 +0300

    removed updated scripts

--- scripts/equake/runnme.sh
+++ scripts/equake/runnme.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-sed 's/config_line_size = 64/config_line_size = '$1'/g' cache-hierarchy_old.simics > cache-hierarchy_buf.simics
-sed 's/config_assoc = 4/config_assoc = '$2'/g' cache-hierarchy_buf.simics > cache-hierarchy.simics
+sed -i 's/ic\.config_line_size =.*$/ic\.config_line_size = '$1'/g' cache-hierarchy.simics
+sed -i 's/ic\.config_assoc = .*$/ic\.config_assoc = '$2'/g' cache-hierarchy.simics
+#sed -i 's/ic\.config_line_size =.*$/ic\.config_line_size = '$1'/g' cache-hierarchy.simics
+#sed -i 's/ic\.config_lines =.*$/ic\.config_lines = '$1'/g' cache-hierarchy.simics
+
+sed -i 's/dc\.config_line_size =.*$/dc\.config_line_size = '$1'/g' cache-hierarchy.simics
+sed -i 's/dc\.config_assoc = .*$/dc\.config_assoc = '$2'/g' cache-hierarchy.simics
+#sed -i 's/dc\.config_line_size =.*$/dc\.config_line_size = '$1'/g' cache-hierarchy.simics
+#sed -i 's/dc\.config_lines =.*$/dc\.config_lines = '$1'/g' cache-hierarchy.simics
+
 sed -i 's/outputfile = .*$/outputfile = ("equake-'$1'.'$2'")/g' equake.simics
 ../../simics-workspace/simics -stall -x equake.simics
