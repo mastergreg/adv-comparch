1174aa04e9063b4fc1c0c05cd899896a5b7c34c4
mastergreg <gregliras@gmail.com>
2012-04-09 13:14:22 +0300

    more data

--- scripts2/mcf/runrunme.sh
+++ scripts2/mcf/runrunme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-./runnme.sh 16 4 32 512
-./runnme.sh 16 4 128 128
-./runnme.sh 32 4 32 1024
+#./runnme.sh 16 4 32 512
+#./runnme.sh 16 4 128 128
+#./runnme.sh 32 4 32 1024
 ./runnme.sh 32 4 128 256
 ./runnme.sh 32 8 32 1024
 ./runnme.sh 32 8 128 256
