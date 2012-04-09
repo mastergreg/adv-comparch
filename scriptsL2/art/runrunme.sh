30d82d8296ba78f8f0df000465bebadd52fb21da
mastergreg <gregliras@gmail.com>
2012-04-09 16:55:16 +0300

    added some art to get from dio

--- scriptsL2/art/runrunme.sh
+++ scriptsL2/art/runrunme.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-./runnme.sh 16 4 32 512
-./runnme.sh 16 4 128 128
-./runnme.sh 32 4 32 1024
-./runnme.sh 32 4 128 256
-./runnme.sh 32 8 32 1024
-./runnme.sh 32 8 128 256
-./runnme.sh 64 4 32 2048
-./runnme.sh 64 4 128 512
-./runnme.sh 64 8 32 2048
-./runnme.sh 64 8 128 512
+./runnme.sh 2048    4
+./runnme.sh 4096    4
+./runnme.sh 4096    8
+./runnme.sh 8192    8
+./runnme.sh 8192    16
+./runnme.sh 16384   8
+./runnme.sh 16384   16
