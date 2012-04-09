ecb217e77374ed1312d0c134e94d72c060c7dae1
mastergreg <gregliras@gmail.com>
2012-04-09 14:20:25 +0300

    clean up the repo

---
+++ scriptsL2/gzip/runnme.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sed -i 's/ic\.config_line_size =.*$/ic\.config_line_size = '$1'/g' cache-hierarchy.simics
+sed -i 's/ic\.config_assoc = .*$/ic\.config_assoc = '$2'/g' cache-hierarchy.simics
+sed -i 's/ic\.config_line_size =.*$/ic\.config_line_size = '$3'/g' cache-hierarchy.simics
+sed -i 's/ic\.config_lines =.*$/ic\.config_lines = '$4'/g' cache-hierarchy.simics
+
+sed -i 's/dc\.config_line_size =.*$/dc\.config_line_size = '$1'/g' cache-hierarchy.simics
+sed -i 's/dc\.config_assoc = .*$/dc\.config_assoc = '$2'/g' cache-hierarchy.simics
+sed -i 's/dc\.config_line_size =.*$/dc\.config_line_size = '$3'/g' cache-hierarchy.simics
+sed -i 's/dc\.config_lines =.*$/dc\.config_lines = '$4'/g' cache-hierarchy.simics
+
+sed -i 's/ts_i\.next_cache_line_size =.*$/ts_i\.next_cache_line_size = '$3'/g' cache-hierarchy.simics
+sed -i 's/ts_d\.next_cache_line_size =.*$/ts_d\.next_cache_line_size = '$3'/g' cache-hierarchy.simics
+
+sed -i 's/outputfile = .*$/outputfile = ("gzip-'$1'.'$2'.'$3'.'$4'")/g' gzip.simics
+../../simics-workspace/simics -stall -x gzip.simics
