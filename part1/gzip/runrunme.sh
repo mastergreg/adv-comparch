27c97f3687b6deefeca32cfd18ddd44079f3bd77
mastergreg <gregliras@gmail.com>
2012-04-10 19:41:20 +0300

    part1 with all the metrics finished

--- part1/gzip/runrunme.sh
+++ part1/gzip/runrunme.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#./runnme.sh 256 2
-#./runnme.sh 256 4
-#./runnme.sh 512 4
-#./runnme.sh 512 8
-#./runnme.sh 1024 4
-./runnme.sh 1024 8
+./runnme.sh 256 2
+./runnme.sh 256 4
+./runnme.sh 512 4
+./runnme.sh 512 8
+./runnme.sh 1024 4
+#./runnme.sh 1024 8
+
