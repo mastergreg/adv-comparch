b64d084ac6a01fc76497dec3b90456ef893ce2c9
Gregory Liras <gregliras@gmail.com>
2012-04-13 19:48:56 +0300

    gplot shows points too, notitle

--- part1b/gzip/proc.sh
+++ part1b/gzip/proc.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-ls | grep $1- | sort -n --field-separator=- -k2 > datafiles
+if [ 'x'$1 == 'x' ];
+then
+    echo "No input given"
+    exit 1
+fi
+if [ -a 'datafiles' ];
+then
+    cat datafiles
+else
+    ls | grep $1- | sort -n --field-separator=- -k2 > datafiles
+    cat datafiles
+fi
 ./ipc.py > ipc-$1
 ./miss-rate.py > miss-rate-$1 
 #rm datafiles 
