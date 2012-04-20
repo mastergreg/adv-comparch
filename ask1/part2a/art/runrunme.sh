13f4abdd4d449d204fcb91c6a14c759e00e8d201
mastergreg <gregliras@gmail.com>
2012-04-20 13:36:38 +0300

    part2a complete

--- ask1/part2a/art/runrunme.sh
+++ ask1/part2a/art/runrunme.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-rm art-*
-./runnme.sh  4  2048   
-./runnme.sh  4  4096   
-./runnme.sh  8  4096   
-./runnme.sh  8  8192   
-./runnme.sh  16 8192   
-./runnme.sh  8  16384  
+#rm art-*
+#./runnme.sh  4  2048   
+#./runnme.sh  4  4096   
+#./runnme.sh  8  4096   
+#./runnme.sh  8  8192   
+#./runnme.sh  16 8192   
+#./runnme.sh  8  16384  
 ./runnme.sh  16 16384  
 ./proc.sh art
