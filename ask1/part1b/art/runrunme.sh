06b40078e9a8b0673ea647d67fc2881eae328197
mastergreg <gregliras@gmail.com>
2012-04-20 13:38:11 +0300

    part 1b complete'

--- ask1/part1b/art/runrunme.sh
+++ ask1/part1b/art/runrunme.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-rm art-*
-./runnme.sh 16 4 32 512
-./runnme.sh 16 4 128 128
-./runnme.sh 32 4 32 1024
-./runnme.sh 32 4 128 256
-./runnme.sh 32 8 32 1024
+#rm art-*
+#./runnme.sh 16 4 32 512
+#./runnme.sh 16 4 128 128
+#./runnme.sh 32 4 32 1024
+#./runnme.sh 32 4 128 256
+#./runnme.sh 32 8 32 1024
 ./runnme.sh 32 8 128 256
 ./runnme.sh 64 4 32 2048
 ./runnme.sh 64 4 128 512
