931a1fbe403e1ee5e0e8030b4663eb7da0816006
Gregory Liras <gregliras@gmail.com>
2012-05-19 21:37:17 +0300

    B1 comple

--- ask2/src/B1/predict.cpp
+++ ask2/src/B1/predict.cpp
@@ -26,68 +26,73 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
 
-	// make sure there is one parameter
+    // make sure there is one parameter
 
-	if (argc != 2) {
-		fprintf (stderr, "Usage: %s <filename>.gz\n", argv[0]);
-		exit (1);
-	}
+    if (argc != 2) {
+        fprintf (stderr, "Usage: %s <filename>.gz\n", argv[0]);
+        exit (1);
+    }
 
-	// open the trace file for reading
+    // open the trace file for reading
 
-	init_trace (argv[1]);
+    init_trace (argv[1]);
 
-	// initialize competitor's branch prediction code
+    // initialize competitor's branch prediction code
 
-	// you can use more than one predictor in an array of predictors!!!
+    // you can use more than one predictor in an array of predictors!!!
 
-	branch_predictor **p = new branch_predictor*[NR_PREDICTORS];
+    branch_predictor **p = new branch_predictor*[NR_PREDICTORS];
 
-    p[0] = btb_predictor(512, 1);
-    p[1] = btb_predictor(256, 2);
-    p[2] = btb_predictor(128, 4);
-    p[3] = btb_predictor(64, 8);
+    p[0] = new btb_predictor(512, 1);
+    p[1] = new btb_predictor(256, 2);
+    p[2] = new btb_predictor(128, 4);
+    p[3] = new btb_predictor(64, 8);
 
-	long long int 
-		tmiss[NR_PREDICTORS],	// number of target mispredictions
-		dmiss[NR_PREDICTORS]; 	// number of direction mispredictions
+    long long int 
+        tmiss[NR_PREDICTORS],	// number of target mispredictions
+        dmiss[NR_PREDICTORS]; 	// number of direction mispredictions
 
     fill( tmiss, tmiss+NR_PREDICTORS, 0);
     fill( dmiss, dmiss+NR_PREDICTORS, 0);
-	// keep looping until end of file
+    // keep looping until end of file
 
-	for (;;) {
+    for (;;) {
 
-		// get a trace
+        // get a trace
 
-		trace *t = read_trace ();
+        trace *t = read_trace ();
 
-		// NULL means end of file
+        // NULL means end of file
 
-		if (!t) break;
+        if (!t) break;
 
-		// send this trace to the competitor's code for prediction
+        // send this trace to the competitor's code for prediction
 
-		branch_update *u;
+        branch_update *u;
         for(int i = 0; i < NR_PREDICTORS; i++) {
             u = p[i]->predict(t->bi);
-            dmiss[i] += u->direction_prediction() != t->taken;
+            if (!(t->bi.br_flags & BR_RETURN)) {
+                dmiss[i] += u->direction_prediction() != t->taken;
+                if((t->taken == u->direction_prediction()) && (u->direction_prediction() == true)) {
+                    tmiss[i] += u->target_prediction() != t->target;
+                }
+            }
             p[i]->update(u, t->taken, t->target);
         }
-	}
-
-	// done reading traces
-
-	end_trace ();
-
-	// give final mispredictions per kilo-instruction and exit.
-	// each trace represents exactly 100 million instructions.
-
-	for(int i = 0; i < NR_PREDICTORS; i++) {
-        printf("%d\t%0.3f\n",i+1,1000.0 * (dmiss[i]/1e8));
-		delete p[i];
     }
-	delete [] p;
 
-	exit (0);
+    // done reading traces
+
+    end_trace ();
+
+    // give final mispredictions per kilo-instruction and exit.
+    // each trace represents exactly 100 million instructions.
+
+    for(int i = 0; i < NR_PREDICTORS; i++) {
+        printf("%d\t%0.3f\n",i+1,1000.0 * (dmiss[i]/1e8));
+        delete p[i];
+    }
+    delete [] p;
+
+    exit (0);
 }
