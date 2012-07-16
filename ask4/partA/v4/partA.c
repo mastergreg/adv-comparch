cb5d7f2c0fdf805fd569a912983557721c50a736
Gregory Liras <gregliras@gmail.com>
2012-07-16 14:19:26 +0300

    added source and simics

---
+++ ask4/partA/v4/partA.c
@@ -0,0 +1,67 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+ * File Name : partA.c
+ * Creation Date : 16-07-2012
+ * Last Modified : Mon 16 Jul 2012 01:50:12 PM EEST
+ * Created By : Greg Liras <gregliras@gmail.com>
+ _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define __MAGIC_CASSERT(p) do {                         \
+    typedef int __check_magic_argument[(p) ? 1 : -1];   \
+} while (0)
+
+#define MAGIC(n) do {                       \
+    __MAGIC_CASSERT(!(n));                  \
+    __asm__ __volatile__ ("xchg %bx,%bx");  \
+} while (0)
+
+#define MAGIC_BREAKPOINT MAGIC(0)
+
+
+
+inline int min(int a, int b)
+{
+    if(a<=b)return a;
+    else return b;
+}
+void init_matrix(float **mat, int n) 
+{    
+    unsigned int i,j;
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+            mat[i][j] = (float)(i+j);
+}
+int main(int argc, char **argv)
+{
+    float **A,**B,**C;
+    int i,j,k,N;
+    N=atoi(argv[1]);
+    A=(float**)malloc(N*sizeof(float*));
+
+    for(i=0; i<N; i++)
+        A[i]=(float*)malloc(N*sizeof(float));
+
+    B=(float**)malloc(N*sizeof(float*));
+
+    for(i=0; i<N; i++)
+        B[i]=(float*)malloc(N*sizeof(float));
+
+    C=(float**)malloc(N*sizeof(float*));
+
+    for(i=0; i<N; i++)
+        C[i]=(float*)malloc(N*sizeof(float));
+
+    fprintf(stderr, "Initializing matrices...\n");
+    init_matrix(A, N);
+    init_matrix(B, N);
+    init_matrix(C, N);
+    MAGIC_BREAKPOINT;
+    for(j=0; j<N; j++)
+        for(k=0; k<N; k++)
+            for(i=0; i<N; i++) {
+                C[i][j] += A[i][k]*B[k][j];
+    }
+    MAGIC_BREAKPOINT;
+    return 0;
+} 
